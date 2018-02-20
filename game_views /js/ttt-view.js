06ae34e49d79ad1b030995fa8320d555a7b4a0d5
Parisa Chavoshi <pchavoshi@gmail.com>
2018-02-20 17:17:51 -0500

    fifth view

--- game_views /js/ttt-view.js
+++ game_views /js/ttt-view.js
@@ -43,8 +43,23 @@ class View {
 
       this.$el.append($figcaption);
   }
+}
 
-//   setupBoard() {}
-// }
+  setupBoard() {
+    const $ul = $("<ul>");
+    $ul.addClass("group");
+
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+      for (let colIdx = 0; colIdx < 3; colIdx++) {
+        let $li = $("<li>");
+        $li.data("pos", [rowIdx, colIdx]);
+
+        $ul.append($li);
+      }
+    }
+
+    this.$el.append($ul);
+  }
+}
 
 module.exports = View;
